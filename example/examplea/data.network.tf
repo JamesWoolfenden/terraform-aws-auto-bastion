3f76df1762bca08983df0e54c96a6cfcf342dd76
James Woolfenden <james.woolfenden@gmail.com>
2026-05-03 20:02:10 +0100

    fix: replace deprecated aws_subnet_ids with aws_subnets, body with response_body

--- example/examplea/data.network.tf
+++ example/examplea/data.network.tf
@@ -1,12 +1,12 @@
 data "aws_vpcs" "vpc" {
 }
-
-data "aws_subnet_ids" "subnets" {
-  vpc_id = element(tolist(data.aws_vpcs.vpc.ids), 0)
+data "aws_subnets" "subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [element(tolist(data.aws_vpcs.vpc.ids), 0)]
+  }
 }
-
 data "http" "myip" {
   url = "http://ipv4.icanhazip.com"
 }
-
 data "aws_caller_identity" "current" {}
